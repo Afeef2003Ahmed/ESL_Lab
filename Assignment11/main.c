0600151d0b019f191d0b196886ed020e809de473
Afeef Ahmed <afeef_ahmed@DESKTOP-ALMK0RT.localdomain>
2026-06-10 18:47:30 +0200

    Updated Assignment 11 main.c

--- Assignment11/main.c
+++ Assignment11/main.c
@@ -8,10 +8,13 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include<time.h>
 
 #define LOOPS 10
 #define SPEED 500000
 #define BYTES 1
+
+
 
 int spiOpen(unsigned spiChan, unsigned spiBaud, unsigned spiFlags) {
   int fd;
@@ -92,13 +95,29 @@ int main(int argc, char *argv[]) {
   if (fd < 0)
     return 1;
 
+  printf("SPI speed: %d Hz  loops: %d  bytes: %d\n\n", speed, loops, bytes);
+  long min_ns  =  999999999L;
+  long max_ns  =  0L;
+  long sum_ns  =  0L;
 
+  
   errors = 0;
   for (i = 0; i < loops; i++) {
     TXBuf[0] = i * 17 + 1;
     expected = ~prev_tx;
 
+    struct timespec t1,t2;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+
     spiXfer(fd, speed, TXBuf, RXBuf, bytes);
+
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+
+    long ns = (t2.tv_sec - t1.tv_sec) * 1000000000L + (t2.tv_nsec - t1.tv_nsec);
+
+    if (ns < min_ns) min_ns = ns;
+    if (ns > max_ns) max_ns = ns;
+    sum_ns += ns;
 
     if ((uint8_t)RXBuf[0] == expected)
       printf("loop %d: sent %d, got %d - correct\n", i, (uint8_t)TXBuf[0], (uint8_t)RXBuf[0]);
@@ -110,6 +129,16 @@ int main(int argc, char *argv[]) {
     prev_tx = TXBuf[0];
     usleep(10000);
   }
+
+  printf("\n========== Timing Summary ==========\n");
+    printf("Speed    : %d Hz\n",          speed);
+    printf("Loops    : %d\n",             loops);
+    printf("Errors   : %d\n",             errors);
+    printf("Min      : %ld ns  (%.2f us)\n", min_ns, min_ns / 1000.0);
+    printf("Max      : %ld ns  (%.2f us)\n", max_ns, max_ns / 1000.0);
+    printf("Average  : %.0f ns  (%.2f us)\n",
+           (double)sum_ns / loops, (double)sum_ns / loops / 1000.0);
+  printf("=====================================\n");
 
   spiClose(fd);
 
